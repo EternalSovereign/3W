f148d64e0fe6a050a048d1c77c808807225db013
EternalSovereign <dipendrakumarbm5@gmail.com>
2025-02-05 19:56:28 +0530

    2nd-publish

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
     "https://3-wfrontend.vercel.app",
+    "https://3-wfrontend-4lfw5cmxw-eternalsovereigns-projects.vercel.app"
 ];
 
 module.exports = allowedOrigins;
